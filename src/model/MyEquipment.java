91cb644d426036e62a9eca3b02dab453027eaf38
Administrator <Administrator@192.168.132.2>
2016-12-27 14:55:20 +0800

    装备的JavaBean和数据库处理

---
+++ src/model/MyEquipment.java
@@ -0,0 +1,61 @@
+package model;
+
+public class MyEquipment {
+	private String id;
+	private String name;
+	private String imgId;
+	private String bd;
+	private String sx;
+
+	public MyEquipment() {		
+		 
+	}
+	public MyEquipment(String id,String name,String imgId,String bd,String sx) {
+		this.id=id;
+		this.name=name;
+		this.imgId=imgId;
+		this.bd=bd;
+		this.sx=sx;	 
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImgId() {
+		return imgId;
+	}
+
+	public void setImgId(String imgId) {
+		this.imgId = imgId;
+	}
+
+	public String getBd() {
+		return bd;
+	}
+
+	public void setBd(String bd) {
+		this.bd = bd;
+	}
+
+	public String getSx() {
+		return sx;
+	}
+
+	public void setSx(String sx) {
+		this.sx = sx;
+	}
+
+}
